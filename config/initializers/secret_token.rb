8faefb21c9df3633ec620596ee5681c87e58c3e7
Tres Matthews <arthur.matthews@employmentguide.com>
2012-08-10 11:19:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICConnect::Application.config.secret_token = '3ee7b90fd4fe8c8d7aec2f82f4d0c7b6781b11204e07e65a07557bddac387017121b30045c197fb34eda1fee12fd2552e833ee81067450ae2f47450a7e7973c1'
